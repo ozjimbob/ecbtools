87a77128b416b59f8cb85a9e38d4efe6e5a4111d
Grant Williamson <grant.williamson@utas.edu.au>
2013-10-15 09:11:03 +1100

    Fixed bug in pointDistSample

--- R/pointDistSample.r
+++ R/pointDistSample.r
@@ -2,8 +2,8 @@ pointDistSample=function(points,sample.size){
   cdist=crossdist(X=coordinates(points)[,1],Y=coordinates(points)[,2],x2=coordinates(points)[,1],y2=coordinates(points)[,2])
   cmn=apply(cdist,1,mean)
   mdist=cmn
-  mdist=(ecbtools::normalize(mdist)/dim(ath)[1])
-  selvec=1:(dim(ath)[1])
+  mdist=(ecbtools::normalize(mdist)/dim(points)[1])
+  selvec=1:(dim(points)[1])
   grb=sample(selvec,size=sample.size,replace=F,prob=mdist)
   new=points[grb,]
   new
