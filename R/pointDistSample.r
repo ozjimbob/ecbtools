6ab54078abaffe0e1ce48dcffcbf32ae4372cd9d
Grant Williamson <grant.williamson@utas.edu.au>
2013-10-18 14:05:23 +1100

    Modified point distance bias sampling function to add square-root transform, and added pointnnSample function that uses nearest-neighbour distances for weighting.  This works better for geographically-dispersed data.

--- R/pointDistSample.r
+++ R/pointDistSample.r
@@ -1,7 +1,8 @@
-pointDistSample=function(points,sample.size){
-  cdist=crossdist(X=coordinates(points)[,1],Y=coordinates(points)[,2],x2=coordinates(points)[,1],y2=coordinates(points)[,2])
+pointDistSample=function(points,sample.size,id=T){
+  cdist=spatstat::crossdist(X=coordinates(points)[,1],Y=coordinates(points)[,2],x2=coordinates(points)[,1],y2=coordinates(points)[,2])
   cmn=apply(cdist,1,mean)
   mdist=cmn
+  if(id==T){mdist=sqrt(mdist)}
   mdist=(ecbtools::normalize(mdist)/dim(points)[1])
   selvec=1:(dim(points)[1])
   grb=sample(selvec,size=sample.size,replace=F,prob=mdist)
@@ -9,3 +10,13 @@ pointDistSample=function(points,sample.size){
   new
 }
 
+pointnnSample=function(points,sample.size,id=T){
+  cdist=spatstat::nndist(X=coordinates(points)[,1],Y=coordinates(points)[,2])
+  mdist=cdist
+  if(id==T){mdist=sqrt(mdist)}
+  mdist=(ecbtools::normalize(mdist)/dim(points)[1])
+  selvec=1:(dim(points)[1])
+  grb=sample(selvec,size=sample.size,replace=F,prob=mdist)
+  new=points[grb,]
+  new
+}
