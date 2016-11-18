5e5ce75b2017cbe494801ec9632de431201c1a5b
Grant Williamson <Grant.Williamson@utas.edu.au>
2016-11-18 12:34:02 +1100

    Stopped raster.kmeans for looking for directories and non-tiff files.

--- R/rasterkmeans.r
+++ R/rasterkmeans.r
@@ -3,7 +3,7 @@ raster.kmeans=function(x,k=12,iter.max=100,nstart=10,geo=T,geo.weight=1){
   if(class(x)[1]=="RasterStack"){
     stk=x
   }else{
-    stl=list.files(x,full.names = TRUE)
+    stl=list.files(x,full.names = TRUE,include.dirs = FALSE,pattern = "tif")
     stk=raster::stack(stl)
   }
   
